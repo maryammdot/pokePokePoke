32e3b1b11e09495b1511583b367365f9b00fa418
Pablo Vega Uceta <pablo.vega@scmspain.com>
2019-04-02 08:34:34 +0200

    Hello world

---
+++ src/components/App/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PokePokePoke from '../../containers/PokePokePoke';
+
+function App() {
+  return <PokePokePoke />;
+}
+
+export default App;
---
+++ src/containers/PokePokePoke/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const pokePokePoke = () => {
+  return <div>Hello World</div>;
+};
+
+export default pokePokePoke;
